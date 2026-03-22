65fa2c68ad8afb072721a8d413b75ec0a4737e1a
Rahul Devrani <devranirahul1@gmail.com>
2026-03-22 21:36:48 +0530

    fixed port for render

--- server.js
+++ server.js
@@ -267,7 +267,7 @@ app.delete('/api/reviews/:id/:userId', (req, res) => {
 
 // server 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log("\nServer is in ");
